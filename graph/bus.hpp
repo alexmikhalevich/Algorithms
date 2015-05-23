b4e2c79dc4798b206e02ce7af582a9184656809d
maked0n <alex.mikhalevich@gmail.com>
2015-05-24 02:32:39 +0300

    Bus problem implementation

--- graph/bus.hpp
+++ graph/bus.hpp
@@ -9,3 +9,25 @@ public:
     CEdge(const int source_id, const int destination_id, const size_t departure, const size_t arrival) :
         CAbstractEdge(source_id, destination_id), e_departure(departure), e_arrival(arrival) {}
 };
+
+class CPath
+{
+private:
+    size_t p_arrival_time;
+public:
+    CPath(const size_t arrival) : p_arrival_time(arrival) {}
+    CPath add(CEdge* edge);
+    bool operator <(const CPath& path);
+};
+
+CPath CPath::add(CEdge* edge) //TODO
+{
+    size_t departure = edge->e_departure;
+}
+
+bool CPath::operator <(const CPath& path)
+{
+    return p_arrival_time < path.p_arrival_time;
+}
+
+//TODO: add schedule class
