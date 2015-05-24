7f34c1f268b1b16c494af910387f50bbb693ad26
maked0n <alex.mikhalevich@gmail.com>
2015-05-25 01:01:11 +0300

    Testing implementation

--- graph/test.cpp
+++ graph/test.cpp
@@ -6,25 +6,25 @@
 TEST(CPathFinderTest, bus_problem)
 {
     BusProblem::CSchedule* schedule = new BusProblem::CSchedule(4);
-    schedule->add(new BusProblem::CEdge(0, 1, 400, 500));
-    schedule->add(new BusProblem::CEdge(1, 2, 600, 630));
-    schedule->add(new BusProblem::CEdge(0, 3, 800, 1000));
+    schedule->add(new BusProblem::CEdge(0, 1, 400, 100, 200));
+    schedule->add(new BusProblem::CEdge(1, 2, 600, 30, 200));
+    schedule->add(new BusProblem::CEdge(0, 3, 800, 200, 400));
     auto pathFinder = init<BusProblem::CEdge, BusProblem::CPath,
             std::vector<BusProblem::CEdge*> (BusProblem::CSchedule::*)(const int) const>(&BusProblem::CSchedule::get, schedule);
-    pathFinder->find_path(new CVertice<BusProblem::CPath>(0), new CVertice<BusProblem::CPath>(2));
+    EXPECT_NO_THROW(pathFinder->find_path(0, 2));
 }
 
 TEST(CPathFinderTest, oriented_graph)
 {
     OrientedGraph::COrientedGraph* graph = new OrientedGraph::COrientedGraph(5);
+    graph->add(new OrientedGraph::CEdge(0, 1, 1));
     graph->add(new OrientedGraph::CEdge(1, 2, 1));
-    graph->add(new OrientedGraph::CEdge(2, 3, 1));
-    graph->add(new OrientedGraph::CEdge(3, 5, 1));
-    graph->add(new OrientedGraph::CEdge(5, 4, 1));
-    graph->add(new OrientedGraph::CEdge(1, 4, 1));
+    graph->add(new OrientedGraph::CEdge(2, 4, 1));
+    graph->add(new OrientedGraph::CEdge(4, 3, 1));
+    graph->add(new OrientedGraph::CEdge(0, 3, 1));
     auto pathFinder = init<OrientedGraph::CEdge, OrientedGraph::CPath,
             std::vector<OrientedGraph::CEdge*> (OrientedGraph::COrientedGraph::*)(const int) const>(&OrientedGraph::COrientedGraph::get, graph);
-    auto path = pathFinder->find_path(new CVertice<OrientedGraph::CPath>(0), new CVertice<OrientedGraph::CPath>(2));
-    EXPECT_EQ(path->get_path_length(), 3);
-    EXPECT_THROW(pathFinder->find_path(new CVertice<OrientedGraph::CPath>(5), new CVertice<OrientedGraph::CPath>(1)), CExNoPath);
+    auto path = pathFinder->find_path(0, 4);
+    EXPECT_EQ(path.get_path_length(), 3);
+    EXPECT_THROW(pathFinder->find_path(4, 0), CExNoPath);
 }
